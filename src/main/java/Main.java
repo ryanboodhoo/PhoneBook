a8038f7d9aabf77d20194c59d1f7d5f8a56fa602
ryanboodhoo <boodho46@gmail.com>
2024-10-26 13:42:51 -0400

    Homework

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        // Q1
+        phoneBook.add("Ryan", "123-456-7890");
+        phoneBook.add("Shawn", "234-567-8901");
+
+        // Q2
+        phoneBook.addAll("Ryan", "345-678-9012", "456-789-0123");
+
+        // Q3
+        phoneBook.remove("Shawn");
+
+        // Q4
+        System.out.println("Has entry for Ryan: " + phoneBook.hasEntry("Ryan"));  // should print true
+        System.out.println("Has entry for Shawn: " + phoneBook.hasEntry("Shawn"));      // should print false
+
+        // Q5
+        List<String> ryanNumbers = phoneBook.lookup("Ryan");
+        System.out.println("Phone numbers for Ryan: " + ryanNumbers);
+
+        // Q6
+        String owner = phoneBook.reverseLookup("345-678-9012");
+        System.out.println("Owner of phone number 345-678-9012: " + owner);
+
+        // Q7
+        List<String> allNames = phoneBook.getAllContactNames();
+        System.out.println("All contact names: " + allNames);
+
+        // Q8
+        Map<String, List<String>> phoneBookMap = phoneBook.getMap();
+        System.out.println("PhoneBook Map: " + phoneBookMap);
+
+
+
+    }
+}
---
+++ src/main/java/PhoneBook.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class PhoneBook {
+    private final Map<String, List<String>> phonebook;//map
+    public PhoneBook(Map<String, List<String>> map) {
+        this.phonebook = map;
+    }
+
+    public PhoneBook() {
+        this(new HashMap<>());
+    } // calling another constructor
+//Q1
+
+    public void add(String name, String phoneNumber) {
+        List<String> numbs = new ArrayList<>();
+        numbs.add(phoneNumber);
+        phonebook.put(name, numbs);
+    }
+
+    //Q2            // ... means multiple
+    public void addAll(String name, String... phoneNumbers) {
+        phonebook.put(name, Arrays.asList(phoneNumbers));
+    }
+    //Q3
+    public void remove(String name) {
+        phonebook.remove(name);
+    }
+    //Q4
+    public Boolean hasEntry(String name) {
+        return phonebook.containsKey(name);
+    }
+    //Q5
+    public List<String> lookup(String name) {
+
+        List<String> numbers = new ArrayList<>(List.of());
+
+        for (Map.Entry<String, List<String>> i : phonebook.entrySet()) {
+            if (i.getKey().contains(name)) {
+                numbers.add(String.valueOf(i.getValue()));
+            }
+        }
+        return numbers;
+    }
+    //Q6
+    public String reverseLookup(String phoneNumber) {
+        String name = "";
+
+        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                name = entry.getKey();
+            }
+        }
+        return name;
+    }
+
+    //Q7
+    public List<String> getAllContactNames() {
+        List<String> names = new ArrayList<>();
+
+        for (Map.Entry<String, List<String>> i : phonebook.entrySet()) {
+            names.add(i.getKey());
+        }
+        return names;
+    }
+    //Q8
+    public Map<String, List<String>> getMap() {
+        Map<String, List<String>> phoneNumber = new HashMap<>();
+
+        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+            phoneNumber.put(entry.getKey(), new ArrayList<>(entry.getValue())); // deep copy
+        }
+
+        return phoneNumber;
+
+
+    }
+}
+
